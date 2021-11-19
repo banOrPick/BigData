41097c75e0b2c14053f7c83c3b3eba7b52b1fce3
hjr <2327553735@qq.com>
2021-11-19 12:08:22 +0800

    add:flink

--- flink/src/main/java/com/banorpick/flink/StreamWorldCount.java
+++ flink/src/main/java/com/banorpick/flink/StreamWorldCount.java
@@ -1,8 +1,11 @@
 package com.banorpick.flink;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
--- flink/src/main/java/com/banorpick/flink/WorldCount.java
+++ flink/src/main/java/com/banorpick/flink/WorldCount.java
@@ -1,11 +1,11 @@
 package com.banorpick.flink;
 
+
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
-import scala.Tuple2;
-
 
 public class WorldCount {
     public static void main(String[] args) throws Exception {
@@ -15,7 +15,7 @@ public class WorldCount {
 
         DataSet<String> inputDataSet = env.readTextFile(inputPath);
 
-        DataSet<scala.Tuple2<String, Integer>> wordCountDataSet = inputDataSet
+        DataSet<Tuple2<String, Integer>> wordCountDataSet = inputDataSet
                 .flatMap(new MyFlatMapper())
                 .groupBy(0)
                 .sum(1);
@@ -24,8 +24,8 @@ public class WorldCount {
     }
 
 
-    private static class MyFlatMapper implements FlatMapFunction<String, scala.Tuple2<String, Integer>> {
-        public void flatMap(String value, Collector<scala.Tuple2<String, Integer>> out) throws Exception {
+    private static class MyFlatMapper implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
             String[] words = value.split(" ");
             for (String word : words) {
                 out.collect(new Tuple2<String, Integer>(word, 1));
